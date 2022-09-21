f6a56216916fe34c964753b0c4c16635fa463223
Ryan Shipley <ryanshipley23@gmail.com>
2022-09-21 14:18:49 -0400

    edits form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+// WE IMPORT OUR COMPONENTS
+import MovieDisplay from "./components/MovieDisplay";
+import Form from "./components/Form";
 
 function App() {
+  // USE OUR COMPONENTS IN APPs RETURNED JSX
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form />
+      <MovieDisplay />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,17 @@
-
 // We Must Import the React Library
 import React from "react";
 
 // Define a function that is our component, always make sure to declare the props parameter so you can use props in your component
 const Form = (props) => {
   //The component must return some JSX
-  return <h1>The Form Component</h1>;
+  return (
+    <div>
+      <form>
+        <input type="text" />
+        <input type="submit" value="submit" />
+      </form>
+    </div>
+  );
 };
 
 // We must export the component to use it in other files
