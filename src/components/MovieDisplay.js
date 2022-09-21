1d7b3a5ca812e61c5e379ab51466daed64b566d0
Ryan Shipley <ryanshipley23@gmail.com>
2022-09-21 14:13:15 -0400

    adds components

---
+++ src/components/MovieDisplay.js
@@ -0,0 +1,12 @@
+
+// We Must Import the React Library
+import React from "react";
+
+// Define a function that is our component, always make sure to declare the props parameter so you can use props in your component
+const MovieDisplay = (props) => {
+  //The component must return some JSX
+  return <h1>The MovieDisplay Component</h1>;
+};
+
+// We must export the component to use it in other files
+export default MovieDisplay;
